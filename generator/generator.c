62aacfde8344a52f12483d7956375c7c10ce3600
Steve Lhomme <slhomme@matroska.org>
2012-05-26 16:45:55 +0200

    the generator is just outputing one folder/file/string per config rather than all combinations add the screen aspect ratio

--- generator/generator.c
+++ generator/generator.c
@@ -9,21 +9,32 @@ static tchar_t *Keyboard[] = {T("keysexposed"), T("keyshidden"), T("keyssoft")};
 static tchar_t *TextInput[] = {T("nokeys"), T("qwerty"), T("12key")};
 static tchar_t *NavigationState[] = {T("navhidden"), T("navexposed")};
 static tchar_t *NavigationMethod[] = {T("nonav"), T("dpad"), T("trackball"), T("wheel")};
+static tchar_t *Aspect[] = {T("long"), T("notlong")};
 
+#if 0
 static void OutputValues(parsercontext *p, size_t size, size_t night, size_t density, size_t touchscreen, size_t keyboard, size_t textinput, size_t navstate, size_t navmethod)
 {
 	tchar_t Path[MAXPATH];
 	textwriter Output, StringOutput;
 
-	stprintf_s(Path, TSIZEOF(Path), T("values-%s-%s-%s-%s-%s-%s-%s-%s"),
-		Size[size],
-		Night[night],
-		Density[density],
-		TouchScreen[touchscreen],
-		Keyboard[keyboard],
-		TextInput[textinput],
-		NavigationState[navstate],
-		NavigationMethod[navmethod]);
+	stprintf_s(Path, TSIZEOF(Path), T("values")
+		T("-%s")
+//		T("-%s")
+		T("-%s")
+//		T("-%s")
+//		T("-%s")
+//		T("-%s")
+//		T("-%s")
+//		T("-%s")
+		,Size[size]
+//		,Night[night]
+		,Density[density]
+//		,TouchScreen[touchscreen]
+//		,Keyboard[keyboard]
+//		,TextInput[textinput]
+//		,NavigationState[navstate]
+//		,NavigationMethod[navmethod]
+		);
 
 	FolderCreate((nodecontext*)p, Path);
 	if (PathIsFolder((nodecontext*)p, Path))
@@ -79,17 +90,63 @@ static void OutputValues(parsercontext *p, size_t size, size_t night, size_t den
 		StreamClose(Output.Stream);
 	}
 }
+#endif
+
+static void OutputArrayValues(parsercontext *p, const tchar_t *name, tchar_t *values[], size_t size)
+{
+	tchar_t Path[MAXPATH];
+	tchar_t Value[MAXPATH];
+	textwriter Output, StringOutput;
+	size_t i;
+
+	stprintf_s(Value, TSIZEOF(Value), T("res_%s"), name);
+
+	for (i=0; i<size; ++i)
+	{
+		stprintf_s(Path, TSIZEOF(Path), T("values-%s"), values[i]);
+
+		FolderCreate((nodecontext*)p, Path);
+		if (PathIsFolder((nodecontext*)p, Path))
+		{
+			tcscat_s(Path, TSIZEOF(Path), T("/resinfo.xml"));
+			memset(&Output,0,sizeof(Output));
+			Output.Stream = StreamOpen(p, Path, SFLAG_WRONLY|SFLAG_CREATE);
+
+			TextElementXML(p, &Output, T("resources"));
+
+			TextElementBegin(&StringOutput, &Output, T("string"));
+			TextAttrib(&StringOutput, T("name"), Value, TYPE_STRING);
+			TcsToUpper(Path, TSIZEOF(Path), values[i]);
+			TextElementEndData(&StringOutput, Path);
+
+			TextElementEnd(&Output);
+
+			StreamClose(Output.Stream);
+		}
+	}
+}
 
 int main(void)
 {
     parsercontext p;
-	size_t size, night, density, touchscreen, keyboard, textinput, navstate, navmethod;
+	//size_t size, night, density, touchscreen, keyboard, textinput, navstate, navmethod;
 
 	// Core-C init phase
 	ParserContext_Init(&p,NULL,NULL,NULL);
 	StdAfx_Init((nodemodule*)&p);
 	//ProjectSettings((nodecontext*)&p);
 
+	OutputArrayValues(&p, T("size"), Size, sizeof(Size)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("night"), Night, sizeof(Night)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("density"), Density, sizeof(Density)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("touchscreen"), TouchScreen, sizeof(TouchScreen)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("keyboard"), Keyboard, sizeof(Keyboard)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("textinput"), TextInput, sizeof(TextInput)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("navstate"), NavigationState, sizeof(NavigationState)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("navmethod"), NavigationMethod, sizeof(NavigationMethod)/sizeof(tchar_t*));
+	OutputArrayValues(&p, T("aspect"), Aspect, sizeof(Aspect)/sizeof(tchar_t*));
+
+#if 0
 	for (size = 0; size < sizeof(Size)/sizeof(tchar_t*); ++size)
 	{
 		for (night = 0; night < sizeof(Night)/sizeof(tchar_t*); ++night)
@@ -115,6 +172,7 @@ int main(void)
 			}
 		}
 	}
+#endif
 
 	// Core-C ending
 	StdAfx_Done((nodemodule*)&p);
